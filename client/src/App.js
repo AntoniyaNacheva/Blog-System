09fd1340e1640f0b43afcf4631facc0d0ced3183
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-11-22 18:47:03 +0200

    Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Auth } from "./pages/auth";
+import { CreatePost } from "./pages/create-post";
+import { PostsList } from "./pages/posts-list";
+import { Navbar } from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
-          <Route path='/' />
+          <Route path='/' element={<Home />} />
+          <Route path='/auth' element={<Auth />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/posts-list' element={<PostsList />} />
         </Routes>
       </Router>
     </div>
