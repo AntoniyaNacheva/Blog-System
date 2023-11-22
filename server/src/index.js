3685c831b62ab75ca1d42b2a45ef7660d472676b
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-11-22 12:07:13 +0200

    User Routes - Register/Login

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import { userRouter } from './routes/users.js';
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect(
 	"mongodb+srv://nachevaantoniya:Blog-System-2023@posts.w1g79td.mongodb.net/posts?retryWrites=true&w=majority"
