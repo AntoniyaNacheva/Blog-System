e05440915ab2d4abaf38ed09fef9f75247b1fba5
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-11-24 10:58:52 +0200

    Create Post Page

--- client/src/pages/create-post.js
+++ client/src/pages/create-post.js
@@ -1,3 +1,57 @@
+import { useState } from "react";
+import axios from "axios";
+
 export const CreatePost = () => {
-	return <div>CreatePost</div>;
+
+	const [post, setPost] = useState({
+		title: "",
+		theme: "",
+		imageUrl: "",
+		author: 0
+	});
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setPost({ ...post, [name]: value });
+	}
+
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			await axios.post("http://localhost:3001/posts", post);
+			alert("Post created!");
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	return (
+		<div className="create-post">
+			<h2> Create Post</h2>
+			<form onSubmit={onSubmit}>
+				<label htmlFor="title">Title</label>
+				<input
+					type="text"
+					id="title"
+					name="title"
+					onChange={handleChange}
+				/>
+				<label htmlFor="theme">Theme</label>
+				<input
+					type="text"
+					id="theme"
+					name="theme"
+					onChange={handleChange}
+				/>
+				<label htmlFor="imageUrl">Image URL</label>
+				<input
+					type="text"
+					id="imageUrl"
+					name="imageUrl"
+					onChange={handleChange}
+				/>
+				<button type="submit">Create Post</button>
+			</form>
+		</div>
+	);
 };
